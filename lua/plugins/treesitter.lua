087f47ae8be9f27a11b523a6c68c0927fa0dfae1
Hans Chaumont <hans.chaumont@gmail.com>
2024-03-02 10:16:20 -0800

    Add markdown and latex support to TreeSitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "zig" },
+      -- see :TSInstallInfo for the list of languages supported
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "zig", "markdown", "latex"},
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },  
