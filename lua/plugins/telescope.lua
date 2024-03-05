5cdc3f556133def1c21de3b85679ed27a2edab19
Hans Chaumont <hans.chaumont@gmail.com>
2024-03-04 19:00:11 -0800

    Add keymap picker to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,6 +20,7 @@ return  {
     -- Find 
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find file'})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope find buffer'})
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope find keymaps'})
     -- Search
     vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope search grep'})
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope search grep'})
