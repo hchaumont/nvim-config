883d51e3ee5c2abd7f443a806de3002bb8aab06a
Hans Chaumont <hans.chaumont@gmail.com>
2024-04-06 21:30:41 -0700

    Adjust formatting in treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,16 @@
 return {
-  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   config = function()
     -- Treesitter setup
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
       -- see :TSInstallInfo for the list of languages supported
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "zig", "markdown", "latex"},
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "zig", "markdown", "latex" },
       sync_install = false,
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
       -- Maybe enable textobjects module in the future
     })
   end
