514bd2a8d4f372a790518874f15de258bdd070c7
Hans Chaumont <hans.chaumont@gmail.com>
2024-05-05 22:49:35 -0700

    Don't use mason for clangd lsp setup

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -13,7 +13,7 @@ require('mason').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  clangd = {},
+  -- clangd = {},
   gopls = {},
   zls = {},
   pyright = {},
