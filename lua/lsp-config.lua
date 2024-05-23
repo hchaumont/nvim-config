6e3eb0c79f90ad362d52fd7f120c39cca13bf3e5
Hans Chaumont <hans.chaumont@gmail.com>
2024-05-23 11:24:28 -0700

    Add LSP autocommands for highlighting the word under the cursor

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -40,6 +40,30 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+  -- Highlight references of the word under the cursor
+  if client and client.server_capabilities.documentHighlightProvider then
+    local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      buffer = bufnr,
+      group = highlight_augroup,
+      callback = vim.lsp.buf.document_highlight,
+    })
+
+    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+      buffer = bufnr,
+      group = highlight_augroup,
+      callback = vim.lsp.buf.clear_references,
+    })
+
+    vim.api.nvim_create_autocmd('LspDetach', {
+      group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+      callback = function(event2)
+        vim.lsp.buf.clear_references()
+        vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+      end,
+    })
+  end
 end
 
 -- mason-lspconfig requires that these setup functions are called in this order
