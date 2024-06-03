843b5d77fc929139ea586ec9ff90c43e09947797
Hans Chaumont <hans.chaumont@gmail.com>
2024-06-03 16:21:29 -0700

    Remove the convenience keymaps for navigating between splits
    I'm already use to using the C-w beforehand anyway, so I'd rather these
    be freed up for something else.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,3 @@
--- Move to window using the <ctrl> hjkl keys
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
-
 -- Resize window using <ctrl> arrow keys
 vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
