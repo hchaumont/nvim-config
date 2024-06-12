19493c8c3eef1fb15e60aaea00d75acf588fab8f
Hans Chaumont <hans.chaumont@gmail.com>
2024-06-12 09:49:16 -0700

    Don't use additional vim regex highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
       -- see :TSInstallInfo for the list of languages supported
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "zig", "markdown", "latex" , "toml", "svelte"},
       sync_install = false,
-      highlight = { enable = true },
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
       indent = { enable = true },
       -- Maybe enable textobjects module in the future
     })
