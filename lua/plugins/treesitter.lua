0d5132fd42f3e74d621c66cb1866a8fe008e7c86
Hans Chaumont <hans.chaumont@gmail.com>
2024-08-18 20:36:55 -0700

    Remove latex from treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,9 +7,13 @@ return {
 
     configs.setup({
       -- see :TSInstallInfo for the list of languages supported
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "zig", "markdown", "latex" , "toml", "svelte"},
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "zig", "markdown", "toml"},
       sync_install = false,
-      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+        disable = {"latex"},
+      },
       indent = { enable = true },
       -- Maybe enable textobjects module in the future
     })
