49695ad49b620abd3083a518d8adfd88b0744f9c
Hans Chaumont <hans.chaumont@gmail.com>
2024-12-20 20:14:08 -0800

    Remove mason
    Mason doesn't like FreeBSD. :(

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,6 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
-    { 'williamboman/mason.nvim', config = true },
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
     -- Useful status updates for LSP
     { 'j-hui/fidget.nvim', opts = {} },
 
