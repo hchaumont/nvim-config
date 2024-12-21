528e52901dd7e79fb211c51bce846989e7145b38
Hans Chaumont <hans.chaumont@gmail.com>
2024-12-20 19:53:31 -0800

    Use gmake to compile telescope-fzf-native
    FreeBSD's make has issues, so use the GNU version instead

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,9 +6,9 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       -- NOTE: If you are having trouble with this installation,
       --       refer to the README for telescope-fzf-native for more instructions.
-      build = "make",
+      build = 'gmake',
       cond = function()
-        return vim.fn.executable("make") == 1
+        return vim.fn.executable 'gmake' == 1
       end,
     },
   },
