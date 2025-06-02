925a8fed6b3a4a0d637d92c3ce88ce579290ccc4
Hans Chaumont <hans.chaumont@gmail.com>
2025-06-01 17:54:36 -0700

    Update treesitter config
    adds typescript and svelte

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,37 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    -- Treesitter setup
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		-- Treesitter setup
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      -- see :TSInstallInfo for the list of languages supported
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "zig", "markdown", "toml"},
-      sync_install = false,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-        disable = {"latex"},
-      },
-      indent = { enable = true },
-      -- Maybe enable textobjects module in the future
-    })
-  end
+		configs.setup({
+			-- see :TSInstallInfo for the list of languages supported
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"typescript",
+				"html",
+				"java",
+				"zig",
+				"markdown",
+				"toml",
+				"svelte",
+			},
+			sync_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+				disable = { "latex" },
+			},
+			indent = { enable = true },
+			-- Maybe enable textobjects module in the future
+		})
+	end,
 }
