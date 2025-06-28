bf9a2b80000e07b78b2f1e8c0084b3026d519457
Hans Chaumont <hans.chaumont@gmail.com>
2025-06-27 17:17:11 -0700

    Fix formatting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,32 @@
-return  {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim',
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
     {
-      'nvim-telescope/telescope-fzf-native.nvim',
+      "nvim-telescope/telescope-fzf-native.nvim",
       -- NOTE: If you are having trouble with this installation,
       --       refer to the README for telescope-fzf-native for more instructions.
-      build = 'make',
+      build = "make",
       cond = function()
-        return vim.fn.executable 'make' == 1
+        return vim.fn.executable("make") == 1
       end,
     },
   },
   config = function()
     -- Enable telescope fzf native, if installed
-    pcall(require('telescope').load_extension, 'fzf')
+    pcall(require("telescope").load_extension, "fzf")
     -- Set bindings for some telescope pickers
     -- See https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#pickers
-    local builtin = require('telescope.builtin')
-    -- Find 
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find file'})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope find buffer'})
-    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope find keymaps'})
+    local builtin = require("telescope.builtin")
+    -- Find
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find file" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffer" })
+    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope find keymaps" })
     -- Search
-    vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = 'Telescope search grep'})
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope search grep'})
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope search help'})
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Telescope search resume'})
-  end
+    vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "Telescope grep buffer" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Telescope search grep" })
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Telescope search help" })
+    vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Telescope search resume" })
+  end,
 }
