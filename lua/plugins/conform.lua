b2b92c566204d54396f689b79c679a64af9c8bcc
Hans Chaumont <hans.chaumont@gmail.com>
2025-06-30 13:44:10 -0700

    Add clang-format to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,30 +1,31 @@
 return {
   { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     keys = {
       {
-        '<leader>cf',
+        "<leader>cf",
         function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
+          require("conform").format({ async = true, lsp_format = "fallback" })
         end,
-        mode = '',
-        desc = '[C]onform [F]ormat buffer',
+        mode = "",
+        desc = "[C]onform [F]ormat buffer",
       },
     },
     opts = {
       notify_on_error = false,
       format_on_save = {},
       formatters_by_ft = {
-        lua = { 'stylua' },
-        zig = { 'zig fmt'},
+        lua = { "stylua" },
+        zig = { "zig fmt" },
 
         -- Conform can also run multiple formatters sequentially
         python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { "npm run format"},
+        javascript = { "npm run format" },
+        c = { "clang_format" },
       },
     },
   },
