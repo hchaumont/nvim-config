c848fd17fa115e4e720a8cfd385e1a6272300728
Hans Chaumont <hans.chaumont@gmail.com>
2026-03-31 22:57:57 -0700

    Remove vim-smoothie
    Got used to jumping

--- lua/plugins/basics.lua
+++ lua/plugins/basics.lua
@@ -1,6 +1,5 @@
 return {
-  'tpope/vim-sleuth',
-  'tpope/vim-surround',
-  'tpope/vim-fugitive',
-  'psliwka/vim-smoothie'
+  "tpope/vim-sleuth",
+  "tpope/vim-surround",
+  "tpope/vim-fugitive",
 }
