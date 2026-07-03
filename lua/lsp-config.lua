94ca9b8c5be9cad7dddded7dc2b79a21db01103d
Hans Chaumont <hans.chaumont@gmail.com>
2026-07-03 16:38:06 -0700

    Drop mason
    I'd rather manage language servers on my own

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,17 +1,12 @@
 -- [[ Configure LSP ]]
-
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
-require('mason').setup()
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
+-- Language servers are expected to be installed and available on the PATH.
 --
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
+-- Enable the following language servers. Add any server-specific settings in
+-- the table below; the value is passed to `vim.lsp.config`. See `:help lsp-config`.
+--
+-- If you want to override the default filetypes that a server attaches to, set
+-- the `filetypes` key (see `html` below).
 local servers = {
   clangd = {},
   gopls = {},
@@ -19,41 +14,30 @@ local servers = {
   pyright = {},
   rust_analyzer = {},
   ts_ls = {},
-  html = { filetypes = { 'html', 'twig', 'hbs'} },
+  html = { filetypes = { "html", "twig", "hbs" } },
   jdtls = {},
+  svelte = {},
 
   lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      diagnostics = { disable = { 'missing-fields' } },
+    settings = {
+      Lua = {
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
+        -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+        diagnostics = { disable = { "missing-fields" } },
+      },
     },
   },
-  svelte = {},
 }
 
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities(capabilities))
+-- nvim-cmp supports additional completion capabilities, so broadcast that to
+-- every server via the `*` wildcard config.
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+vim.lsp.config("*", { capabilities = capabilities })
 
--- You can add other tools here that you want Mason to install
--- for you, so that they are available from within Neovim.
-local ensure_installed = vim.tbl_keys(servers or {})
-vim.list_extend(ensure_installed, {
-  'stylua', -- Used to format Lua code
-})
-require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
-
-require('mason-lspconfig').setup({
-  handlers = {
-    function(server_name)
-      local server = servers[server_name] or {}
-      -- This handles overriding only values explicitly passed
-      -- by the server configuration above. Useful when disabling
-      -- certain features of an LSP (for example, turning off formatting for ts_ls)
-      server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-      require('lspconfig')[server_name].setup(server)
-    end,
-  },
-})
+-- Apply per-server settings, then enable them all. Server defaults (cmd,
+-- root markers, filetypes) come from the `lsp/` configs shipped by nvim-lspconfig.
+for name, config in pairs(servers) do
+  vim.lsp.config(name, config)
+end
+vim.lsp.enable(vim.tbl_keys(servers))
